52524c185fb8a4ca517f3ad6967fd88a2ee5859f
srodenas <srodher115@g.educaand.es>
2025-02-20 14:24:00 +0100

    Sigo modificando

--- src/main/kotlin/com/domain/usecase/InsertEmployeeUseCase.kt
+++ src/main/kotlin/com/domain/usecase/InsertEmployeeUseCase.kt
@@ -2,8 +2,16 @@ package com.domain.usecase
 
 import com.domain.models.Employee
 import com.domain.repository.EmployeeInterface
-import kotlinx.coroutines.CoroutineStart
-import kotlin.io.encoding.Base64
+import com.ktor.ApplicationContext
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import javax.imageio.ImageIO
+
 
 class InsertEmployeeUseCase  (val repository : EmployeeInterface){
 
@@ -22,7 +30,7 @@ class InsertEmployeeUseCase  (val repository : EmployeeInterface){
         else{
             val img = employee!!.urlImage
             img?.let{
-                employee!!.urlImage = reateBase64ToImg(it)  //creamos la imagen, a partir del Base64 y devolvemos su http
+                employee!!.urlImage = createBase64ToImg(it, employee!!.dni)  //creamos la imagen, a partir del Base64 y devolvemos su http
             }
             //aquí tengo que tener la imagen creada y el name en employee!!.urlImage
             val new = repository.postEmployee(employee!!)
@@ -36,7 +44,7 @@ class InsertEmployeeUseCase  (val repository : EmployeeInterface){
     /*
     Esta función, recibe el base64 u otra cosa.
      */
-    private fun createBase64ToImg(img: String) : String?{
+    private fun createBase64ToImg(img: String , dni: String) : String?{
         /*
         primer grupo. sacamos el tipo de la imagen.
         segiundo grupo. sacamos el cuerpo.
@@ -47,10 +55,23 @@ class InsertEmployeeUseCase  (val repository : EmployeeInterface){
         return if (result != null) {
                     //todo ya tengo que crear la imagen
                     val type = result.groupValues[1]
+                    val ext: String = type.split("/")[1]  //nos quedamos sólo con la extensión.
                     val body = result.groupValues[2]
+                    if (ext != "jpg" || ext != "jpeg" || ext != "gif")
+                        return null
                     try {
-                        val imgBytes = Base64.decode(img, CoroutineStart.DEFAULT)
-
+                        val imgBytes = Base64.getDecoder().decode(img)  //decodificamos y convertimos a ByteArray
+                        val inputStream = ByteArrayInputStream(imgBytes) //Convertimos el array de bytes a flujo de datos
+                        val bufferImage:BufferedImage = ImageIO.read(inputStream)  //A partir del flujo de entrada (imagen en bytes), convertimos a imagen.
+                        val path : String = ApplicationContext.context.environment.config.property("ktor.path.images") + "/{dni}"
+                        val dir = File(path)
+                        if (dir.isDirectory){
+                            val nameFile: String = path+"/{$dni}_${SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())}.$ext"
+                            //ya podemos crear el fichero.
+                            ImageIO.write(bufferImage, ext, nameFile)
+                        }else{
+                            return null
+                        }
                     }catch (e : Exception) {
                         e.printStackTrace()
                     }
